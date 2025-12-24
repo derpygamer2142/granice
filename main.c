92e759ff6b5bd37a4eaf7edce3ac57142ab4bd89
derpygamer2142 <93667155+derpygamer2142@users.noreply.github.com>
2025-12-24 12:03:20 -0800

    simple http server

--- main.c
+++ main.c
@@ -1,34 +1,102 @@
+// networking headers
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
-#include <ifaddrs.h>
+#include <unistd.h>
+#include <errno.h>
+
+// program headers
+#include <time.h>
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main() {
-    struct ifaddrs * addresses;
-    if (getifaddrs(&addresses) == -1) {
-        printf("getifaddrs call failed\n");
-        return -1;
+    printf("Configuring local address\n");
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_family = AF_INET6;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE;
+    
+    struct addrinfo *bind_address;
+    getaddrinfo(0, "8080", &hints, &bind_address);
+
+    printf("Creating socket\n");
+    int socket_listen;
+    socket_listen = socket(bind_address->ai_family,
+        bind_address->ai_socktype, bind_address->ai_protocol);
+    
+    if (socket_listen < 0) {
+        fprintf(stderr, "socket() failed. (%d)\n", errno);
+        return 1;
     }
 
-    struct ifaddrs *address = addresses;
-    while (address) {
-        int family = address->ifa_addr->sa_family;
-        if (family == AF_INET || family == AF_INET6) {
-            printf("%s\t", address->ifa_name);
-            printf("%s\t", family == AF_INET ? "IPv4" : "IPv6");
-            
-            char ap[100];
-            const int family_size = family == AF_INET ? 
-                sizeof(struct sockaddr_in) : sizeof(struct sockaddr_in6);
-            
-            getnameinfo(address->ifa_addr, family_size, ap, sizeof(ap), 0, 0, NI_NUMERICHOST);
-            printf("\t%s\n", ap);
-        }
-
-        address = address->ifa_next;
+    // this makes it so ipv4 gets mapped to ipv6
+    // doesn't work on wsl so i can't test it
+    int option = 0;
+    if (setsockopt(socket_listen, IPPROTO_IPV6, IPV6_V6ONLY, (void*)&option, sizeof(option))) {
+        fprintf(stderr, "setsockopt() failed. (%d)\n", errno);
+        return 1;
     }
 
-    freeifaddrs(addresses);
+    printf("Binding socket\n");
+    if (bind(socket_listen, bind_address->ai_addr, bind_address->ai_addrlen)) {
+        fprintf(stderr, "bind( failed. (%d)\n", errno);
+        return 1;
+    }
+    freeaddrinfo(bind_address);
+
+    printf("Now listening\n");
+    if (listen(socket_listen, 10)) {
+        fprintf(stderr, "listen() failed. (%d)\n", errno);
+        return 1;
+    }
+
+    printf("Waiting for connection\n");
+    struct sockaddr_storage client_address;
+    socklen_t client_len = sizeof(client_address);
+    int socket_client = accept(socket_listen, (struct sockaddr*) &client_address, &client_len);
+    if (socket_client < 0) {
+        fprintf(stderr, "accept() failed. (%d)\n", errno);
+        return 1;
+    }
+
+    printf("Client connected\n");
+    char address_buffer[100];
+    getnameinfo((struct sockaddr*)&client_address, client_len, address_buffer,
+                sizeof(address_buffer), 0, 0, NI_NUMERICHOST);
+    printf("Client address: %s\n", address_buffer);
+
+    printf("Reading request\n");
+    char request[1024];
+    int bytes_received = recv(socket_client, request, sizeof(request), 0);
+    printf("Received %d bytes\n", bytes_received);
+    printf("Request: %.*s", bytes_received, request);
+
+    printf("Sending response\n");
+    const char *response =
+        "HTTP/1.1 200 OK\r\n"
+        "Connection: close\r\n"
+        "Content-Type: text/plain\r\n\r\n"
+        "Time is: ";
+    int bytes_sent = send(socket_client, response, strlen(response), 0);
+    printf("Sent %d of %d bytes\n", bytes_sent, (int)strlen(response));
+
+    time_t timer;
+    time(&timer);
+    char *time_msg = ctime(&timer);
+    bytes_sent = send(socket_client, time_msg, strlen(time_msg), 0);
+    printf("Sent %d of %d bytes\n", bytes_sent, (int)strlen(time_msg));
+
+    printf("Closing connection\n");
+    close(socket_client);
+
+    printf("Closing listening socket\n");
+    close(socket_listen);
+
+    printf("Done\n");
+
     return 0;
 }
